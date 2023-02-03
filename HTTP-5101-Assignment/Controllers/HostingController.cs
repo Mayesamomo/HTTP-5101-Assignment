2dc34399695cc0b6b3e61d529e242c47913e8ee8
Michael Momo <softakcess@gmail.com>
2023-02-03 18:42:14 -0500

    rounding up

---
+++ HTTP-5101-Assignment/Controllers/HostingController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HTTP_5101_Assignment.Controllers
+{
+
+    public class HostingController : ApiController
+    {
+        /// <summary>
+        /// Calculates the total cost of hosting and maintenance.
+        /// </summary>
+        /// <param name="id">The number of days counted since the beginning of the hosting</param>
+        /// <returns>
+        /// The total cost as a currency string.The total cost as a currency string.
+        /// </returns>
+
+        [Route("api/hosting/hostingcost/{id}")]
+        [HttpGet]
+        public string getHostingCost(int id)
+        {
+            
+            // variables
+            double baseCost = 5.50; //cost per fortnight {1 fortnight = 14 days}
+            double HST = 0.13; 
+            double totalCost = (id / 14) * baseCost * (1 + HST);
+            if( id >=0 && id < 14)
+            {
+                totalCost = totalCost + 1;
+            }
+
+            else if(id == 14)
+            { 
+                totalCost =totalCost + 2;
+            }
+            return  "Total Cost: " + totalCost.ToString();
+
+        }
+    }
+}
--- HTTP-5101-Assignment/Controllers/MathController.cs
+++ HTTP-5101-Assignment/Controllers/MathController.cs
@@ -10,13 +10,26 @@ namespace HTTP_5101_Assignment.Controllers
    
     public class MathController : ApiController
     {
+        /// <summary>
+        ///  returns arithmetical simplifcation of the number entered.
+        ///  
+        /// </summary>
+        /// <param name="id">The parameter that takes in user input</param>
+        /// <returns></returns>
         [HttpGet]
-        [Route("api/math/{id}")]
-        public int Get(int id)
+        [Route("api/math/NumberMachine/{id}")]
+        public string MathOperation(int id)
         {
             //Todo: summary and finish up the method
-            return id; 
+            int result1 = id + id; //adds the nummber entered by thesame number 
+            int result2 = id * id; // multiplies the input
+            int result3 = id - id; //subtracts the input
+            int result4 = id / id; //divides the inputs
 
+            string answer = " the operation returns:  " + " \n Addition Operation" +
+                result1 +"\n muliplicattion Operation"  + result2 + "\n subtraction  Operation: " + "\n division Operation: " + result3
+                + "\n" + "\n" + result4;
+            return answer;
 
         }
     }
